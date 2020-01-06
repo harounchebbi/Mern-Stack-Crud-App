112c87bae930e26f823af3af4c793bb7dcd46cc3
Haroun <mohamedharoun.chebbi@esprit.tn>
2020-01-06 09:32:51 +0100

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // Allow Promises
 mongoose.Promise = global.Promise;
 // Connection
-mongoose.connect('mongodb://localhost:27017/gomycode_test', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/db_test', { useNewUrlParser: true });
 // Validation
 mongoose.connection
   .once('open', () => console.log('Connected to the database!'))
