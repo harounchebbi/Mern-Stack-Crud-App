390761897c8ddf9a934afd1cf6f3f0c4061f1a01
Aman <patilaman080@gmail.com>
2020-11-30 18:52:49 +0530

    fixed some decaprecated warning issues and updated the PUT-Route

--- routes/students.js
+++ routes/students.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Student model
-const Students = require('../models/students');
+const Students = require("../models/students");
 
 // @route   GET /api/students/
 // @desc    Get all students
 // @access  Public
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const students = await Students.find({});
-    res.send({ students })
-  } catch(err) {
+    res.send({ students });
+  } catch (err) {
     res.status(400).send({ error: err });
   }
 });
@@ -19,36 +19,53 @@ router.get('/', async (req, res) => {
 // @route   GET /api/students/:id
 // @desc    Get a specific student
 // @access  Public
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const student = await Students.findById(req.params.id);
     res.send({ student });
   } catch (err) {
-    res.status(404).send({ message: 'Student not found!' });
+    res.status(404).send({ message: "Student not found!" });
   }
 });
 
 // @route   POST /api/students/
 // @desc    Create a student
 // @access  Public
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const newStudent = await Students.create({ name: req.body.name, email: req.body.email, enrollnumber: req.body.enrollnumber });
-     res.send({ newStudent });
-  } catch(err) {
+    const newStudent = await Students.create({
+      name: req.body.name,
+      email: req.body.email,
+      enrollnumber: req.body.enrollnumber,
+    });
+    res.send({ newStudent });
+  } catch (err) {
     res.status(400).send({ error: err });
   }
-
 });
 
 // @route   PUT /api/students/:id
 // @desc    Update a student
 // @access  Public
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "enrollnumber"];
+  const isValid = updates.every((val) => {
+    return allowedUpdates.includes(val);
+  });
+  if (!isValid) {
+    return res.status(400).send({
+      error:
+        "Invalid Operation You can Only Update [ name,email,enrollnumber ]",
+    });
+  }
   try {
-    const updatedStudent = await Students.findByIdAndUpdate(req.params.id, req.body);
-     res.send({ message: 'The student was updated' });
-  } catch(err) {
+    const updatedStudent = await Students.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.send({ message: "The student was updated" });
+  } catch (err) {
     res.status(400).send({ error: err });
   }
 });
@@ -56,14 +73,13 @@ router.put('/:id', async (req, res) => {
 // @route   DELETE /api/students/:id
 // @desc    Delete a student
 // @access  Public
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const removeStudent = await Students.findByIdAndRemove(req.params.id);
-     res.send({ message: 'The student was removed' });
-  } catch(err) {
+    res.send({ message: "The student was removed" });
+  } catch (err) {
     res.status(400).send({ error: err });
   }
 });
-
 
 module.exports = router;
